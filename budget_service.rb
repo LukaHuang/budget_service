44666cc4bd72289cd475477056eb20b09d7ec374
Steven Huang <szteven3@gmail.com>
2020-05-02 16:10:38 +0800

    change interface

--- budget_service.rb
+++ budget_service.rb
@@ -1,12 +1,14 @@
+require 'date'
+
 class BudgetService
   attr_accessor :start_time
   attr_accessor :end_time
-  def initialize(start_time:, end_time:)
-    @start_time = start_time
-    @end_time = end_time
+  def initialize()
   end
 
-  def query
+  def query(start_time:, end_time:)
+    @start_time = start_time
+    @end_time = end_time
     return 310
   end
 
--- budget_service_test.rb
+++ budget_service_test.rb
@@ -4,7 +4,10 @@ require_relative 'budget_service'
 
 class BudgetServiceTest < Minitest::Test
   def test_budget_can_return
-    result = BudgetService.new(start_time: 20200501, end_time: 20200531).query
+    result = BudgetService.new.query(start_time: '20200501', end_time: '20200531')
     assert_equal result, 310
   end
+
+
+
 end
